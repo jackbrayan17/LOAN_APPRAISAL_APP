ae2e4fd4b59cfa7e0ec9de477b118f6eeed51157
Jack Brayan <122466921+jackbrayan17@users.noreply.github.com>
2025-05-15 23:51:44 +0100

    Delete music_ai/music_ai_project directory

--- music_ai/music_ai_project/music_ai_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class UserPreferences(models.Model):
-    preferred_genre = models.CharField(max_length=100)
-    favorite_artists = models.TextField()
-
-    def __str__(self):
-        return self.preferred_genre
